c47aae10c2f9e6ede0c18c3f931157e2c7f810f9
andersjr1984 <46797989+andersjr1984@users.noreply.github.com>
2019-01-17 15:43:26 -0700

    Update euler_223.py

--- euler_223.py
+++ euler_223.py
@@ -1,3 +1,4 @@
+#this problem utilizes the pythagorean triple matrix to produce a count of triangles with perimeters less than the given input.
 def branch_1 (x,y,z,N):
     a = x - 2*y + 2*z
     b = 2*x - y + 2*z
@@ -41,13 +42,19 @@ def branch_3 (x,y,z,N):
         count += 1
     return count
 
-for t in range(int(input())):
-    N = int(input())
-    br_1_test = int(N/12)+1
-    count = int((N-1)/2)
-    while br_1_test > 0:
-        count += branch_2(1,br_1_test,br_1_test,N)
-        if br_1_test != 1:
-            count += branch_3(1,br_1_test,br_1_test,N)
-        br_1_test -= 1
-    print (count)
+prompt = "Give me a value for the max perimeter and I will give you the number of triangles that have a perimeter less than your input and meet the equation: a**2+b**2=c**2+1: "
+N = int(input(prompt))
+
+#the program only needs to go through N/12+1 iterations.
+br_1_test = int(N/12)+1
+
+#this provides a count of all the triangles with a=1.  If we use the matrix method, it will count both a=1 and b=1 triangles as the same.
+count = int((N-1)/2)
+
+#start at the top and work backwards
+while br_1_test > 0:
+    count += branch_2(1,br_1_test,br_1_test,N)
+    if br_1_test != 1:
+        count += branch_3(1,br_1_test,br_1_test,N)
+    br_1_test -= 1
+print (count)
