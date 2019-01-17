467e183e48981b4153eda945bf7f8de72c4cc5fb
andersjr1984 <46797989+andersjr1984@users.noreply.github.com>
2019-01-17 15:55:30 -0700

    Create euler_224.py

---
+++ euler_224.py
@@ -0,0 +1,42 @@
+def branch_1 (x,y,z,N):
+    a = x - 2*y + 2*z
+    b = 2*x - y + 2*z
+    c = 2*x - 2*y + 3*z
+    count = 0
+    if (a+b+c <= N):
+        count += branch_1(a,b,c,N)
+        count += branch_2(a,b,c,N)
+        count += branch_3(a,b,c,N)
+        if (a<=b):
+            count +=1
+    return count
+
+def branch_2(x,y,z,N):
+    a = x+2*y+2*z
+    b = 2*x+y+2*z
+    c = 2*x+2*y+3*z
+    count = 0
+    if (a+b+c <= N):
+        count += branch_1(a,b,c,N)
+        count += branch_2(a,b,c,N)
+        count += branch_3(a,b,c,N)
+        if (a<=b):
+            count += 1
+    return count
+
+def branch_3 (x,y,z,N):
+    a = -x+2*y+2*z
+    b = -2*x+y+2*z
+    c = -2*x+2*y+3*z
+    count=0
+    if (a+b+c <= N):
+        count += branch_1(a,b,c,N)
+        count += branch_2(a,b,c,N)
+        count += branch_3(a,b,c,N)
+        if (a<=b):
+            count += 1
+    return count
+
+for t in range(int(input())):
+    N = int(input())
+    print (1+branch_1(2,2,3,N) + branch_2(2,2,3,N) + branch_3(2,2,3,N))
